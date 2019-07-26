2128fb09d859c399b74ced091d38552468c6e0aa
Sarah Tatro <statrox4@gmail.com>
2019-07-26 16:43:11 -0600

    Refactor order_item_spec after adding one to many relationship between orders and addresses

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -13,9 +13,14 @@ RSpec.describe OrderItem do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
-      @order_1 = @user.orders.create!
-      @order_2 = @user.orders.create!
+      @alex = User.create!(name: "Alex Hennel", email: "straw@gmail.com", password: "fish", role: 0)
+      @berry = User.create!(name: "Berry Blue", email: "blue@gmail.com", password: "bear", role: 1, merchant_id: @megan.id)
+      @jeff = User.create!(name: "Jeff Casimir", email: "jeff@gmail.com", password: "jeff", role: 2)
+      @alex_work = Address.create!(nickname: "work", address: "123 Straw Lane", city: "Straw City", state: "CO", zip: 12345, user_id: @alex.id)
+      @berry_home = Address.create!(nickname: "home", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, user_id: @berry.id)
+      @jeff_home = Address.create!(nickname: "home", address: "345 Blue Lane", city: "Blue City", state: "CA", zip: 56789, user_id: @jeff.id)
+      @order_1 = @alex.orders.create!(address_id: @alex_work.id)
+      @order_2 = @alex.orders.create!(address_id: @alex_work.id)
       @order_item_1 = @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2)
       @order_item_2 = @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 3)
       @order_item_3 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 27)
