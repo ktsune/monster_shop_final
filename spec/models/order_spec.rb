1c067d0968fe1c6dbe718d98776e8545963e6af5
Sarah Tatro <statrox4@gmail.com>
2019-07-26 16:34:03 -0600

    Refactor order_spec after adding one to many relationship between orders and addresses

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe Order do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-      # @user = User.create!(name: 'Megan', email: 'megan_1@example.com', password: 'securepassword')
 
       @alex = User.create!(name: "Alex Hennel", email: "straw@gmail.com", password: "fish", role: 0)
       @berry = User.create!(name: "Berry Blue", email: "blue@gmail.com", password: "bear", role: 1, merchant_id: @megan.id)
@@ -28,8 +27,6 @@ RSpec.describe Order do
       @order_1 = @alex.orders.create!(address_id: @alex_work.id, status: "packaged")
       @order_2 = @berry.orders.create!(address_id: @berry_home.id, status: "pending")
 
-      # @order_1 = @user.orders.create!(status: "packaged")
-      # @order_2 = @user.orders.create!(status: "pending")
       @order_item_1 = @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 5, fulfilled: true)
       @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
       @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
@@ -83,7 +80,7 @@ RSpec.describe Order do
     before :each do
       @alex = User.create!(name: "Alex Hennel", email: "straw@gmail.com", password: "fish", role: 0)
       @alex_work = Address.create!(nickname: "work", address: "123 Straw Lane", city: "Straw City", state: "CO", zip: 12345, user_id: @alex.id)
-      
+
       @order_1 = @alex.orders.create!(status: 1, address_id: @alex_work.id)
       @order_2 = @alex.orders.create!(status: 0, address_id: @alex_work.id)
       @order_3 = @alex.orders.create!(status: 3, address_id: @alex_work.id)
