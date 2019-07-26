f8ebcfee9e1434b4dab85b44c639b6c8c55ab62f
Sarah Tatro <statrox4@gmail.com>
2019-07-26 13:24:31 -0600

    Install rspec and shoulda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
+    # binding.pry
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
