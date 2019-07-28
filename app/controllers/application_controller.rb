d0e9aa03d574aa54a197349abb1b4089962d0731
Sarah Tatro <statrox4@gmail.com>
2019-07-28 13:42:51 -0600

    Fix application controller test coverage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_user?
+    current_user&.default?
+  end
+
   def current_merchant_user?
     current_user && current_user.merchant_id
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,29 @@ RSpec.describe User do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
   end
-end
+
+  describe User do
+      describe 'roles' do
+        it 'can be created as a admin user' do
+          user = User.create(email: 'joe', password: 'combat', role: 2)
+
+          expect(user.role).to eq('admin')
+          expect(user.admin?).to be_truthy
+        end
+
+        it 'can be created as merchant user' do
+          user = User.create(email: 'joe', password: 'combat', role: 1)
+
+          expect(user.role).to eq('merchant_admin')
+          expect(user.merchant_admin?).to be_truthy
+        end
+
+        it 'can be created as a default user' do
+          user = User.create(email: 'sammy', password: 'pass')
+
+          expect(user.role).to eq('default')
+          expect(user.default?).to be_truthy
+        end
+      end
+    end
+  end
